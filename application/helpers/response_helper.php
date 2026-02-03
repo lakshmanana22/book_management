ea84c1a323941adbeb7b4ae7659b052af7008a80
lakshmanana22 <lakshmanana22@gmail.com>
2026-02-03 19:17:56 +0530

    changes

--- application/helpers/response_helper.php
+++ application/helpers/response_helper.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 function json_response($data, $code = 200) {
     $CI =& get_instance();
@@ -16,22 +15,3 @@ function json_error($message, $code = 400) {
         'message' => $message
     ], $code);
 }
-=======
-<?php
-function json_response($data, $code = 200) {
-    $CI =& get_instance();
-    $CI->output
-        ->set_status_header($code)
-        ->set_content_type('application/json')
-        ->set_output(json_encode($data))
-        ->_display();
-    exit;
-}
-
-function json_error($message, $code = 400) {
-    json_response([
-        'success' => false,
-        'message' => $message
-    ], $code);
-}
->>>>>>> 4211d5297b3990ed1c4035e9f8a639de7a717852
--- application/models/Book_model.php
+++ application/models/Book_model.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -63,69 +62,3 @@ class Book_model extends CI_Model {
 
 
 }
-=======
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Book_model extends CI_Model {
-
-    // public function getAll() {
-    //     return $this->db->get('books')->result();
-    // }
-
-    public function create($data) {
-        return $this->db->insert('books', $data);
-    }
-
-    public function findById($id) {
-        return $this->db->get_where('books', ['id' => $id])->row();
-    }
-
-    public function updateBook($id, $data) {
-        return $this->db->where('id', $id)->update('books', $data);
-    }
-
-    public function deleteBook($id) {
-        return $this->db->where('id', $id)->delete('books');
-    }
-
-    public function getPaginated($filters, $limit, $offset) {
-
-        if (!empty($filters['title'])) {
-            $this->db->like('title', $filters['title']);
-        }
-
-        if (!empty($filters['author'])) {
-            $this->db->like('author', $filters['author']);
-        }
-
-        if (!empty($filters['genre'])) {
-            $this->db->where('genre', $filters['genre']);
-        }
-
-        return $this->db
-            ->limit($limit, $offset)
-            ->get('books')
-            ->result();
-    }
-
-    public function countAll($filters) {
-
-        if (!empty($filters['title'])) {
-            $this->db->like('title', $filters['title']);
-        }
-
-        if (!empty($filters['author'])) {
-            $this->db->like('author', $filters['author']);
-        }
-
-        if (!empty($filters['genre'])) {
-            $this->db->where('genre', $filters['genre']);
-        }
-
-        return $this->db->count_all_results('books');
-    }
-
-
-}
->>>>>>> 4211d5297b3990ed1c4035e9f8a639de7a717852
